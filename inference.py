8b8937d1b46f0ae08996b479b1dc3788b54df932
enesyaman <enesyaman@hacettepe.edu.tr>
2023-12-16 02:13:06 +0300

    Calculating roof area from Google Maps (with using scale bar) implemented on scale_bar.py. Basic changes on inference.py for use these calculations

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import time
 
 import cv2
 import numpy as np
@@ -8,6 +9,8 @@ import torch
 from torch import nn
 from transformers import SegformerFeatureExtractor
 from transformers import SegformerForSemanticSegmentation
+
+import scale_bar
 
 
 class Model:
@@ -63,26 +66,25 @@ class Model:
         return id2color, id2name, name2id
 
     def predict(self, image):
-        pixel_values = self.preprocessing(image)
-        logits = self.forward_pass(pixel_values)
-        height, width, _ = self.get_image_shape(image)
-        # First, rescale logits to original image size
-        upsampled_logits = nn.functional.interpolate(logits, size=(height, width),  # (height, width)
-                                                     mode='bilinear', align_corners=False)
-        # Second, apply argmax on the class dimension
-        seg = upsampled_logits.argmax(dim=1)[0]
+        preprocessed_image = self.preprocessing(image)
+        logits = self.forward_pass(preprocessed_image)
+        height, width, _ = image.shape
+        upsampled_logits = torch.nn.functional.interpolate(logits,
+                                                        size=(height,width),
+                                                        mode="bilinear",
+                                                        align_corners=False)
 
-        color_seg = np.zeros_like(image, dtype=np.uint8)  # height, width, 3
+        seg = upsampled_logits.argmax(dim=1)[0].cpu()
+        color_seg =  np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
         if self.id2color is None:
-            id2color, id2name, name2id = self.read_color_map()
+            self.id2color, self.id2name, self.name2id = self.read_color_map()
 
-        for label, color in id2color.items():
+        for label, color in self.id2color.items():
             if not label == 0:
                 color_seg[seg == label, :] = color
-        # Convert to BGR
         color_seg = color_seg[..., ::-1]
         output_image = self.merge_original_image_width_segmentation_outputs(image, color_seg)
-        return output_image
+        return output_image, seg
 
     def inference_image(self, image_path):
         images = glob.glob(image_path)
@@ -90,18 +92,20 @@ class Model:
         for image_path in images:
             im_name = os.path.basename(image_path)
             image = cv2.imread(image_path)
-            output_image = self.predict(image)
-
-            if self.record_outputs:
-                cv2.imwrite(f"{self.recording_path}/{im_name}.png", output_image)
-                counter += 1
+            output_image, mask = self.predict(image)
+            cv2.imwrite(f"{self.recording_path}/{im_name}", output_image)
+            for label, name in self.id2name.items():
+                pixel_meter_ratio = scale_bar.find_length_scale_bar_web(image, int(im_name[:-5]))
+                area = scale_bar.calc_mask_area(mask.numpy(), label, pixel_meter_ratio)
+                print(f'{name} : {area} meter squared')
+            counter += 1
 
 
 if __name__ == "__main__":
     model_path = "model_outdir/checkpoint-610"
     color_map_path = "segmentation_dataset/colormap.txt"  # Path of the colormap.txt (probably in datasets)
     output_path = 'outputs'
-    image_path = "inputs/*.png"  # The image-images path for want to inference (RegEx)
+    image_path = "scale_inputs/*.png"  # The image-images path for want to inference (RegEx)
 
     model = Model(model_path, color_map_path, output_path=output_path)
     model.inference_image(image_path)
---
+++ scale_bar.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+
+def find_length_scale_bar_web(image, meter):
+    # Convert to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Set the threshold to detect all white areas in the image
+    _, binary_image = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
+
+    # Detect all contours
+    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # We expect the scale bar to be near the bottom of the image
+    lower_third = binary_image[-20:, :]
+    # Detect all contours in the lower third of the image
+    scale_bar_contours, _ = cv2.findContours(lower_third, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find the contour that corresponds to the scale bar based on expected characteristics
+    scale_bar_contours = sorted(scale_bar_contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+    _, _, w, h = cv2.boundingRect(scale_bar_contours[-1])
+    if w > h * 5:  # This is a simple heuristic and may need adjustment
+        return meter / w
+    else:
+        raise Exception('Bulamadım abi')
+
+
+def calc_mask_area(mask, label, pixel_meter_ratio):
+    # Count the number of pixels that match the specified label
+    pixel_count = np.sum(mask == label)
+
+    # Calculate the real-world area
+    area = pixel_count * pixel_meter_ratio ** 2
+
+    return area
